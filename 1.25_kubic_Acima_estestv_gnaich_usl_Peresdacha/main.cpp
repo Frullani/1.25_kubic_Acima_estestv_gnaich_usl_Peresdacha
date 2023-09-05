73da434d9063c147c13a43d8d3297221a5e75cf3
Frullani <35868245+Frullani@users.noreply.github.com>
2023-09-05 14:28:04 +0300

    Initial Commit

---
+++ 1.25_kubic_Acima_estestv_gnaich_usl_Peresdacha/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1.25_kubic_Acima_estestv_gnaich_usl_Peresdacha
+//
+//  Created by Arsenii Klevakin on 05.09.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
